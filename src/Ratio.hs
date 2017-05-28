6b894d39fc18bb414460bd47f9c7ce0fab9fdf48
pmilne <philip@pmilne.net>
2017-05-28 13:12:09 -0700

    Add test

--- src/Ratio.hs
+++ src/Ratio.hs
@@ -8,10 +8,10 @@ infix  6  :/
 data Ratio a = !a :/ !a deriving (Eq, Show, Read)
 
 instance  (Ring a) => Ring (Ratio a) where
-    (r1 :/ i1) + (r2 :/ i2)   =  (r1 + r2) :/ (i1 + i2)
-    (r1 :/ i1) * (r2 :/ i2)   =  (r1 * r2 - i1 * i2) :/ (r1 * i2 + i1 * r2)
-    (r1 :/ i1) ^ (r2 :/ i2)   =  undefined
-    negate (r:/i)             =  negate r :/ i
+    (n1 :/ d1) + (n2 :/ d2)   =  (n1 + n2) :/ (d1 + d2)
+    (n1 :/ d1) * (n2 :/ d2)   =  (n1 * n2 - d1 * d2) :/ (n1 * d2 + d1 * n2)
+    (n1 :/ d1) ^ (n2 :/ d2)   =  undefined
+    negate (n :/ d)             =  negate n :/ d
     zero     = zero :/ one
     one     = one :/ one
 --    promote a = a :/ 0
