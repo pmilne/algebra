6ee84e435b4df3c25c3c3ab7b280432c3f32532e
pmilne <philip@pmilne.net>
2017-05-28 14:54:46 -0700

    Tidy

--- src/Euclidean.hs
+++ src/Euclidean.hs
@@ -4,9 +4,6 @@ class Euclidean a where
     quo      :: a -> a -> a
     rem      :: a -> a -> a
     gcd      :: a -> a -> a
-    quo       = undefined
-    rem       = undefined
-    gcd       = undefined
 
 instance Euclidean Int where
     quo       = Prelude.quot
