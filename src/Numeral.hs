8cb8fb67c2402012f664e27d67fce97624eb7523
pmilne <philip@pmilne.net>
2017-05-28 20:06:54 -0700

    Tidy

--- src/Numeral.hs
+++ src/Numeral.hs
@@ -5,41 +5,9 @@ import Ring
 -- The Church Numeral type
 newtype Numeral t
   = Numeral ((t -> t) -> (t -> t))
---  = Numeral1 (Int -> Int)
---  = Numeral1 ((t -> t) -> (t -> t))
---        deriving (Eq, Show, Read)
-
---cinc :: Integer -> Integer
---cinc x = x + 1
-
---unchurch :: ((s -> s) -> (s -> s)) -> Integer
---unchurch :: ((a -> a) -> a -> a) -> Integer
---unchurch n = n cinc (0 :: Integer)
-
-
---unchurch2 :: ((Integer -> Integer) -> t) -> t
---unchurch2 n = n cinc
-
---unchurch3 :: Num t => ((Integer -> Integer) -> t -> t1) -> t1
---unchurch3 :: ((s -> s) -> (s -> s)) -> Integer
---unchurch3 n = unchurch2 n (0 :: Integer)
-
---instance Show (Numeral1 s) where
---   show (Numeral1 n1) = show (unchurch3 n1)
 
 instance (Show s, Num s) => Show (Numeral s) where
    show (Numeral n1) = show (n1 (Prelude.+1) 0)
-
---instance (Show a, Num a) => Show ((a -> a) -> a -> a) where
---    show n = show $ n (+1) 0
-
-instance  (Num a) => Num (Numeral a) where
-    (Numeral n1) + (Numeral n2) = Numeral (\f x -> (n1 f (n2 f x)))
-    (Numeral n1) * (Numeral n2) = Numeral (\f -> n1 (n2 f))
-    negate (Numeral n1)         =  undefined
-    abs z                       =  undefined
-    signum z                    =  undefined
-    fromInteger n               =  undefined
 
 instance  (Ring a) => Ring (Numeral a) where
     (Numeral n1) + (Numeral n2) = Numeral (\f x -> (n1 f (n2 f x)))
@@ -82,21 +50,3 @@ times1 a b f = a (b f)
 --expt1 :: t -> (t -> t) -> t
 expt1 :: Numeral2 t -> Numeral2 (t -> t) -> Numeral2 t
 expt1 a b = b a
-
-{-
-pow :: Numeral t -> Numeral t -> Numeral t
--- pow :: t -> (t -> u) -> u
-pow a n = n a
--}
-
-{-
-pow :: Church a -> Church a -> Church a
-pow a n = n
-
-instance Show (Church a) where
-	show x = show (unchurch x)
-
-instance Show ((a -> a) -> a -> a) where
-	show x = show (unchurch x)
--}
-
