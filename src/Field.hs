f35366e93e621bca1624ba3d0baf0982b03a912d
pmilne <philip@pmilne.net>
2017-05-28 19:32:17 -0700

    Tidy

--- src/Field.hs
+++ src/Field.hs
@@ -8,8 +8,8 @@ infixr 7 /
 class Ring a => Field a where
     (/)      :: a -> a -> a
     inv      :: a -> a
-    x / y               = x * inv y
-    inv x               = one Field./ x
+    x / y     = x * inv y
+    inv x     = one Field./ x
 
 instance Field Double where
     (/)       = (Prelude./)
