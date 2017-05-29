8572c7faa83374117d8d9c2ae46630697454fed5
pmilne <philip@pmilne.net>
2017-05-29 00:22:14 -0700

    Tidy

--- src/Expression.hs
+++ src/Expression.hs
@@ -98,9 +98,9 @@ derivative (Var _)           = one
 derivative (Neg f)           = Neg (derivative f)
 derivative (Sum a b)         = derivative a + derivative b
 derivative (Prd a b)         = a * derivative b + b * derivative a --product rule (ab' + a'b)
-derivative (Div a b)         = (derivative a * b - a * derivative b) / Pow b (Const (one + one)) -- quotient rule ( (a'b - b'a) / b^2 )
-derivative (Pow a (Const x)) = Const x * derivative a * Pow a (Const (x - one)) --specialised power rule (xa^(x-1) * a')
-derivative (Pow f g)         = Pow f g * (derivative f * g / f + derivative g * log f) --general power rule: https://en.wikipedia.org/wiki/Differentiation_rules#Generalized_power_rule
+derivative (Div a b)         = (derivative a * b - a * derivative b) / b ^ Const (one + one) -- quotient rule ( (a'b - b'a) / b^2 )
+derivative (Pow a (Const x)) = Const x * derivative a * a ^ Const (x - one) --specialised power rule (xa^(x-1) * a')
+derivative (Pow f g)         = f ^ g * (derivative f * g / f + derivative g * log f) --general power rule: https://en.wikipedia.org/wiki/Differentiation_rules#Generalized_power_rule
 derivative (Log a)           = derivative a / a
 
 ddx :: (Eq a, Field a, Exponentiable a) => Expression a -> Expression a
