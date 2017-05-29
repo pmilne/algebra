0276714f2f9cc2508d42b0c3b611f06ec9e0c84a
pmilne <philip@pmilne.net>
2017-05-29 07:50:12 -0700

    Tidy

--- src/Complex.hs
+++ src/Complex.hs
@@ -12,13 +12,13 @@ conjugate        :: Ring a => Complex a -> Complex a
 conjugate (x :+ y) =  x :+ negate y
 
 instance (Ring a) => Ring (Complex a) where
-    (r1 :+ i1) + (r2 :+ i2)   =  (r1 + r2) :+ (i1 + i2)
-    (r1 :+ i1) * (r2 :+ i2)   =  (r1 * r2 - i1 * i2) :+ (r1 * i2 + i1 * r2)
-    negate (r :+ i)           =  negate r :+ negate i
-    zero                      = zero :+ zero
-    one                       = one  :+ zero
+    (r1 :+ i1) + (r2 :+ i2) = (r1 + r2) :+ (i1 + i2)
+    (r1 :+ i1) * (r2 :+ i2) = (r1 * r2 - i1 * i2) :+ (r1 * i2 + i1 * r2)
+    negate (r :+ i)         = negate r :+ negate i
+    zero                    = zero :+ zero
+    one                     = one  :+ zero
 --    promote a = a :+ 0
 
 instance (Field a) => Field (Complex a) where
-    inv (a :+ b)             =  let d = a * a + b * b in a / d :+ negate b / d
+    inv (a :+ b)            =  let d = a * a + b * b in a / d :+ negate b / d
 
