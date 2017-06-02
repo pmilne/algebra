8f2d7c67ae7d405c3250c2b222848cf61f42f42e
pmilne <philip@pmilne.net>
2017-06-01 23:49:30 -0700

    Tidy

--- src/Polynomial.hs
+++ src/Polynomial.hs
@@ -16,20 +16,6 @@ instance (Show a) => Show (Polynomial a) where
     show (Term a n r)   = show a ++ " * x ^ " ++ show n ++ " + " ++ show r
 
 instance (Ring a) => Ring (Polynomial a) where
---    p1 + p2 = case p1 of Const a1       -> case p2 of Const a2      -> Const (a1 + a2)
---                                                      Term a2 n2 r2 -> Term a2 n2 (p1 + r2)
---                         Term a1 n1 r1  -> case p2 of Const a2      -> Term a1 n1 (r1 + p2)
---                                                      Term a2 n2 r2 | n1 > n2 -> Term a1 n1 (r1 + p2)
---                                                                    | n1 < n2 -> Term a2 n2 (p1 + r2)
---                                                                    | otherwise -> Term (a1 + a2) n1 (r1 + r2)
-
---    p1@(Const a1)      + p2@(Const a2)      = Const (a1 + a2)
---    p1@(Const a1)      + p2@(Term a2 n2 r2) = Term a2 n2 (p1 + r2)
---    p1@(Term a1 n1 r1) + p2@(Const a2)      = Term a1 n1 (r1 + p2)
---    p1@(Term a1 n1 r1) + p2@(Term a2 n2 r2) | n1 > n2   = Term a1 n1 (r1 + p2)
---                                            | n1 < n2   = Term a2 n2 (p1 + r2)
---                                            | otherwise = Term (a1 + a2) n1 (r1 + r2)
-
     Const a1      + Const a2                = Const (a1 + a2)
     Const a1      + Term a2 n2 r2           = Term a2 n2 (Const a1 + r2)
     Term a1 n1 r1 + Const a2                = Term a1 n1 (r1 + Const a2)
@@ -37,10 +23,10 @@ instance (Ring a) => Ring (Polynomial a) where
                                             | n1 < n2   = Term a2 n2 (p1 + r2)
                                             | otherwise = Term (a1 + a2) n1 (r1 + r2)
 
-    p1@(Const a1)      * p2@(Const a2)      = Const (a1 * a2)
-    p1@(Const a1)      * p2@(Term a2 n2 r2) = Term (a1 * a2) n2 (p1 * r2)
-    p1@(Term a1 n1 r1) * p2@(Const a2)      = Term (a1 * a2) n1 (r1 * p2)
-    p1@(Term a1 n1 r1) * p2@(Term a2 n2 r2) = mult a1 n1 p2 + r1 * p2;
+    Const a1      * Const a2      = Const (a1 * a2)
+    Const a1      * Term a2 n2 r2 = Term (a1 * a2) n2 (Const a1 * r2)
+    Term a1 n1 r1 * Const a2      = Term (a1 * a2) n1 (r1 * Const a2)
+    Term a1 n1 r1 * p2            = mult a1 n1 p2 + r1 * p2;
 
     negate (Term a n r)      = Term (negate a) n (negate r)
 
