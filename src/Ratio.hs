572903305103f3874a3de4e5ae418e2eeae22376
pmilne <philip@pmilne.net>
2017-06-03 10:46:27 -0700

    Tidy

--- src/Ratio.hs
+++ src/Ratio.hs
@@ -9,7 +9,7 @@ import Field
 data Ratio a = Ratio !a !a deriving (Eq, Show, Read)
 
 canonical :: (Euclidean a) => a -> a -> Ratio a
-canonical n d = let g = gcd n d in Ratio (quo n g) (quo d g)
+canonical n d = let g = gcd n d in Ratio (quo n g) (quo d g) -- fix sign!!!!
 
 instance (Ring a, Euclidean a) => Additive (Ratio a) where
     (Ratio n1 d1) + (Ratio n2 d2) = canonical (n1 * d2 + n2 * d1) (d1 * d2)
@@ -23,4 +23,4 @@ instance (Ring a, Euclidean a) => Ring (Ratio a) where
 --    promote a = a :/ 0
 
 instance (Ring a, Euclidean a) => Field (Ratio a) where
-    inv (Ratio n d) = Ratio d n -- fixme sign!!!!
+    inv (Ratio n d) = Ratio d n -- fix sign!!!!
