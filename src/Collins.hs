8ea78235c45a3da3c4e9126d30692d91dd8a09dd
pmilne <philip@pmilne.net>
2017-06-03 15:09:26 -0700

    Tidy

--- src/Collins.hs
+++ src/Collins.hs
@@ -8,22 +8,15 @@ import Polynomial
 
 infixr 8 ^
 
-powerAssociative ::
-   (a -> a -> a) -> a -> a -> Integer -> a
+powerAssociative :: (a -> a -> a) -> a -> a -> Integer -> a
 powerAssociative _  a0 _ 0 = a0
-powerAssociative op a0 a n =
-   powerAssociative op
-      (if even n then a0 else op a0 a)
-      (op a a) (div n 2)
+powerAssociative op a0 a n = powerAssociative op (if even n then a0 else op a0 a) (op a a) (div n 2)
 
 (^) :: Ring a => a -> Integer -> a
 a ^ n = powerAssociative (*) one a n
 
 pseudoRem :: (Ring a, Euclidean a) => Polynomial a -> Polynomial a -> a -> Integer -> Polynomial a
-pseudoRem u v lcv delta =
-        let ff = lcv ^ (delta Prelude.+ 1) in
-        let su = Const ff * u in
-        rem su v
+pseudoRem u v lcv delta = rem (Const (lcv ^ (delta Prelude.+ 1)) * u) v
 
 subresultant :: (Eq a, Ring a, Euclidean a) => Polynomial a -> Polynomial a -> a  -> a  -> (Polynomial a -> a -> r) -> r
 -- u.deg >= v.deg
@@ -39,7 +32,6 @@ subresultant u (Const lcv) g h =
         let delta = degree u in
         let nh = divideOrFail (lcv ^ delta) (h ^ (delta - 1)) in
         \f -> f (Const lcv) nh
-
 subresultant u v@(Term lcv degv redV) g h =
         let delta = degree u - degv in
         let pRem = pseudoRem u v lcv delta in
