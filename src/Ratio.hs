16560906a6a9a78da7911fdb2ab4320417c23c74
pmilne <philip@pmilne.net>
2017-06-03 10:37:59 -0700

    Tidy

--- src/Ratio.hs
+++ src/Ratio.hs
@@ -9,7 +9,7 @@ import Field
 data Ratio a = Ratio !a !a deriving (Eq, Show, Read)
 
 frac :: (Euclidean a) => a -> a -> Ratio a
-frac n d = let g = gcd n d in Ratio (quo n g) (quo d  g)
+frac n d = let g = gcd n d in Ratio (quo n g) (quo d g)
 
 instance (Ring a, Euclidean a) => Additive (Ratio a) where
     (Ratio n1 d1) + (Ratio n2 d2)   = frac (n1 * d2 + n2 * d1) (d1 * d2)
