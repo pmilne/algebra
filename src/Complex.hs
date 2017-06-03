7976ef0dacc5517e8cab09057a7abbd251e55df9
pmilne <philip@pmilne.net>
2017-06-03 10:02:44 -0700

    Add a (rather unsatisfactory) promote function

--- src/Complex.hs
+++ src/Complex.hs
@@ -16,11 +16,8 @@ data Complex a = !a :+ !a deriving (Eq, Show, Read)
 conjugate :: Ring a => Complex a -> Complex a
 conjugate (x :+ y) =  x :+ negate y
 
-promote1 :: (Additive a) => a -> Complex a
-promote1 a = a :+ zero
-
 instance (Additive a) => Composite (Complex a) a where
-    promote n = promote n
+    promote a = a :+ zero
 
 instance (Additive a) => Additive (Complex a) where
     (r1 :+ i1) + (r2 :+ i2) = (r1 + r2) :+ (i1 + i2)
