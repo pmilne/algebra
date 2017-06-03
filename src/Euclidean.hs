29b8e078578b2601dcfeccd74063f4c1cdf88720
pmilne <philip@pmilne.net>
2017-06-03 11:08:16 -0700

    Tidy

--- src/Euclidean.hs
+++ src/Euclidean.hs
@@ -1,12 +1,14 @@
 module Euclidean where
 
 class Euclidean a where
-    quo      :: a -> a -> a
-    rem      :: a -> a -> a
-    gcd      :: a -> a -> a
+--    quo       :: a -> a -> a
+--    rem       :: a -> a -> a
+--    gcd       :: a -> a -> a
+    canonical :: a -> a -> (a -> a -> b) -> b
 
 instance Euclidean Int where
-    quo       = Prelude.quot
-    rem       = Prelude.rem
-    gcd       = Prelude.gcd
+--    quo           = Prelude.quot
+--    rem           = Prelude.rem
+--    gcd           = Prelude.gcd
+    canonical n d = let g = Prelude.signum d * Prelude.gcd n d in \f -> f (Prelude.quot n g) (Prelude.quot d g)
 
--- src/Ratio.hs
+++ src/Ratio.hs
@@ -8,19 +8,16 @@ import Field
 
 data Ratio a = Ratio !a !a deriving (Eq, Show, Read)
 
-canonical :: (Euclidean a) => a -> a -> Ratio a
-canonical n d = let g = gcd n d in Ratio (quo n g) (quo d g) -- fix sign!!!!
-
 instance (Ring a, Euclidean a) => Additive (Ratio a) where
-    (Ratio n1 d1) + (Ratio n2 d2) = canonical (n1 * d2 + n2 * d1) (d1 * d2)
+    (Ratio n1 d1) + (Ratio n2 d2) = canonical (n1 * d2 + n2 * d1) (d1 * d2) Ratio
     zero                          = Ratio zero one
 --    promote a = a :/ 0
 
 instance (Ring a, Euclidean a) => Ring (Ratio a) where
-    (Ratio n1 d1) * (Ratio n2 d2) = canonical (n1 * n2) (d1 * d2)
+    (Ratio n1 d1) * (Ratio n2 d2) = canonical (n1 * n2) (d1 * d2) Ratio
     negate (Ratio n d)            = Ratio (negate n) d
     one                           = Ratio one one
 --    promote a = a :/ 0
 
 instance (Ring a, Euclidean a) => Field (Ratio a) where
-    inv (Ratio n d) = Ratio d n -- fix sign!!!!
+    inv (Ratio n d) = canonical d n Ratio -- this could be optimised; there is no need for a gcd in this case
