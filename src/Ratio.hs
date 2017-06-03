fe2f517eff2bf2074f5e76922d783014cc6a68d9
pmilne <philip@pmilne.net>
2017-06-03 10:41:41 -0700

    Tidy

--- src/Ratio.hs
+++ src/Ratio.hs
@@ -12,8 +12,8 @@ frac :: (Euclidean a) => a -> a -> Ratio a
 frac n d = let g = gcd n d in Ratio (quo n g) (quo d g)
 
 instance (Ring a, Euclidean a) => Additive (Ratio a) where
-    (Ratio n1 d1) + (Ratio n2 d2)   = frac (n1 * d2 + n2 * d1) (d1 * d2)
-    zero                      = Ratio zero one
+    (Ratio n1 d1) + (Ratio n2 d2) = frac (n1 * d2 + n2 * d1) (d1 * d2)
+    zero                          = Ratio zero one
 --    promote a = a :/ 0
 
 instance (Ring a, Euclidean a) => Ring (Ratio a) where
