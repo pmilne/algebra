9997c8dcfa3326112141a8766917fd896162b868
pmilne <philip@pmilne.net>
2017-06-09 09:38:54 -0700

    Tidy

--- test/TestExpression.hs
+++ test/TestExpression.hs
@@ -14,27 +14,27 @@ instance Exponentiative (Rational a) where
   a ^ b = undefined
   log a = undefined
 
-var :: Expression (Rational Integer)
-var = Var "x"
+x :: Expression (Rational Integer)
+x = Var "x"
 
 pc :: (Euclidean a, Ring a) => a -> Expression (Rational a)
 pc x = Const (rational1 x)
 
 xp1 :: Expression (Rational Integer)
-xp1 = var + pc 1
+xp1 = x + pc 1
 
 e0, e1, e2, e3 :: Expression (Rational Integer)
 --e0 = xp1 * xp1 -- (x + 1)^2
 e0 = xp1 -- (x + 1)
-e1 = pc 3 * Pow var (pc 2) -- 3x^2
-e2 = Pow var var -- x ^ x
-e3 = Log (Log var)
+e1 = pc 3 * Pow x (pc 2) -- 3x^2
+e2 = Pow x x -- x ^ x
+e3 = Log (Log x)
 
 d0, d1, d2, d3 :: Expression (Rational Integer)
 d0 = pc 1
-d1 = pc 6 * var -- 6x
-d2 = Pow var var * (pc 1 + Log var)
-d3 = pc 1 / (var * Log var)
+d1 = pc 6 * x -- 6x
+d2 = Pow x x * (pc 1 + Log x)
+d3 = pc 1 / (x * Log x)
 
 run :: IO ()
 run = do
