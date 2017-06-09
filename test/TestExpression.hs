b3ea450abb49663b37cc73a0ee448b964138afb3
pmilne <philip@pmilne.net>
2017-06-08 18:35:08 -0700

    More on tests.

--- test/TestExpression.hs
+++ test/TestExpression.hs
@@ -11,17 +11,24 @@ import TestUtil
 var :: Expression Double
 var = Var 'x'
 
-expr :: Expression Double
---expr = Const 3 * Pow var (Const 2) -- 3x^2
-expr = Pow var var -- x ^ x
+xp1 = (var + Const 1)
 
-dexpr :: Expression Double
---dexpr = Const 6 * var -- 6x
-dexpr = Pow var var * (Expression.Const 1.0 + Log var)
+e0, e1, e2 :: Expression Double
+--e0 = xp1 * xp1 -- (x + 1)^2
+e0 = xp1 -- (x + 1)
+e1 = Const 3 * Pow var (Const 2) -- 3x^2
+e2 = Pow var var -- x ^ x
+
+d0, d1, d2 :: Expression Double
+d0 = Const 1
+d1 = Const 6 * var -- 6x
+d2 = Pow var var * (Expression.Const 1.0 + Log var)
 
 run :: IO ()
 run = do
-          putStrLn ("expr = " ++ show expr)
-          test "derivative expr" dexpr (derivative expr)
-          putStrLn ("eval (ddx expr) = " ++ show (evalExpr 'x' 5 (derivative expr)))
-          putStrLn ("expr = " ++ show expr)
+          test ("derivative " ++ show e0) d0 (derivative e0)
+          test ("derivative " ++ show e1) d1 (derivative e1)
+          test ("derivative " ++ show e2) d2 (derivative e2)
+
+          putStrLn ("eval (ddx expr) = " ++ show (evalExpr 'x' 5 (derivative e2)))
+          putStrLn ("expr = " ++ show e2)
