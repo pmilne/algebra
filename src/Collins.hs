771d9d5f923818c8a7d38637bf7f62802ba56193
pmilne <philip@pmilne.net>
2017-06-10 09:41:48 -0700

    Tidy

--- src/Collins.hs
+++ src/Collins.hs
@@ -24,8 +24,8 @@ pseudoRem u v lcv delta = rem (Const (lcv ^ (delta + 1)) * u) v
 -- Translated from Knuth Volume II: "The Subresultant Algorithm", Section 4.6.1
 -- degree u >= degree v
 subresultant :: (Show a, Eq a, Ring a, Euclidean a) => (Polynomial a -> a -> r) -> Polynomial a -> Polynomial a -> r
-subresultant rtn u0 v0 =
-        rec u0 v0 one one where -- avoid passing the rtn function in the recursion
+subresultant rtn u v =
+        (if deg u > deg v then rec u v else rec v u) one one where -- avoid passing the rtn function in the recursion
         rec u v g h =
              trace ("subresultant " ++ "\t\tu: " ++ show u ++ "\t\tv: " ++ show v ++ "\t\tv: " ++ show g ++ "\t\tv: " ++ show h) $
              if v == zero then
@@ -40,14 +40,11 @@ subresultant rtn u0 v0 =
 --                subresultant f v (divideOrFail (pseudoRem u v lcv delta) (Const (g * (h ^ delta)))) lcv nh -- also works
                   rec v (divideOrFail2 (pseudoRem u v lcv delta) (g * (h ^ delta))) lcv nh
 
-gcdAndResultant :: (Show a, Eq a, Ring a, Euclidean a) => (Polynomial a -> a -> r) -> Polynomial a -> Polynomial a -> r
-gcdAndResultant rtn u v = if deg u > deg v then subresultant rtn u v else subresultant rtn v u
-
 resultant :: (Show a, Eq a, Ring a, Euclidean a) => Polynomial a -> Polynomial a -> a
-resultant = gcdAndResultant (\_ r -> r)
+resultant = subresultant (\_ r -> r)
 
 gcd1 :: (Show a, Eq a, Ring a, Euclidean a) => Polynomial a -> Polynomial a -> Polynomial a
-gcd1 = gcdAndResultant (\g _ -> g)
+gcd1 = subresultant (\g _ -> g)
 
 instance (Show a, Eq a, Ring a, Euclidean a) => Euclidean (Polynomial a) where
     divide          = divide1
