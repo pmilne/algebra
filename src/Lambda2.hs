86873a9fcf68fa6ee43af19acc67026c3641484d
pmilne <philip@pmilne.net>
2017-06-16 20:02:31 +0100

    Try to generify Expression...

--- src/Lambda2.hs
+++ src/Lambda2.hs
@@ -7,15 +7,16 @@ Translated from original Java sources here: https://github.com/pmilne/lambda/blo
 
 module Lambda2 where
 
+import Prelude hiding (exp)
 import Data.List
 
-data (Fun a) = Fun {name_ :: String, function_ :: (Primitive a) -> (Primitive a)}
+data (Fun a) = Fun {name_ :: String, function_ :: Primitive a -> Primitive a}
 
 instance Eq (Fun a) where
-  Fun n1 f1 == Fun n2 f2 = n1 == n2
+  Fun n1 _ == Fun n2 _ = n1 == n2
 
 instance Show (Fun a) where
-  show (Fun n f) = n
+  show (Fun n _) = n
 
 data (Primitive a) = Int0 Int
                | Str0 String
@@ -33,22 +34,22 @@ varName :: Expression a -> String
 varName (Symbol s) = s
 varName _ = error "variable wasn't a variable!!!"
 
-toFunction :: (Primitive a) -> (Fun a)
+toFunction :: Primitive a -> Fun a
 toFunction (Fun0 f) = f
-toFunction p = undefined
+toFunction _ = undefined
 
-toFunction2 :: (Primitive a) -> ((Primitive a) -> (Primitive a))
+toFunction2 :: Primitive a -> Primitive a -> Primitive a
 toFunction2 f = function_ (toFunction f)
 
-toInt :: (Primitive a) -> Int
+toInt :: Primitive a -> Int
 toInt (Int0 i) = i
-toInt p = undefined
+toInt _ = undefined
 
 getOrFail :: Maybe a -> a
 getOrFail (Just x) = x
 getOrFail Nothing = error "This didin't happen"
 
-createCompiler :: [String] -> Expression a -> [(Primitive a)] -> (Primitive a)
+createCompiler :: [String] -> Expression a -> [Primitive a] -> Primitive a
 createCompiler nameStack {-exp-} =
     rec {-exp-} where
     rec exp = case exp of
@@ -64,11 +65,11 @@ createCompiler nameStack {-exp-} =
                         let arg0 = rec arg in
                         \env -> toFunction2 (fun0 env) (arg0 env)
 
-                    Lambda var exp ->
+                    Lambda var body ->
                         let var0 = varName var in
-                        let exp0 = createCompiler (var0 : nameStack) exp in
+                        let exp0 = createCompiler (var0 : nameStack) body in
                         \env -> Fun0 (Fun "hello" (\arg -> exp0 (arg : env)))
 
 
-eval :: Expression a -> (Primitive a)
+eval :: Expression a -> Primitive a
 eval input = createCompiler [] input []
