5cf7de2b827cf14039f533b8dc0f70f274d23f7f
pmilne <philip@pmilne.net>
2017-06-16 19:56:46 +0100

    Try to generify Expression...

--- src/Lambda2.hs
+++ src/Lambda2.hs
@@ -9,21 +9,21 @@ module Lambda2 where
 
 import Data.List
 
-data Fun = Fun {name_ :: String, function_ :: Primitive -> Primitive}
+data (Fun a) = Fun {name_ :: String, function_ :: (Primitive a) -> (Primitive a)}
 
-instance Eq Fun where
+instance Eq (Fun a) where
   Fun n1 f1 == Fun n2 f2 = n1 == n2
 
-instance Show Fun where
+instance Show (Fun a) where
   show (Fun n f) = n
 
-data Primitive = Int0 Int
+data (Primitive a) = Int0 Int
                | Str0 String
-               | Fun0 Fun
+               | Fun0 (Fun a)
                deriving (Eq, Show)
 
 
-data Expression a = Constant !Primitive
+data Expression a = Constant !(Primitive a)
                 | Symbol String
                 | Lambda !(Expression a)!(Expression a)
                 | Application !(Expression a) !(Expression a)
@@ -33,14 +33,14 @@ varName :: Expression a -> String
 varName (Symbol s) = s
 varName _ = error "variable wasn't a variable!!!"
 
-toFunction :: Primitive -> Fun
+toFunction :: (Primitive a) -> (Fun a)
 toFunction (Fun0 f) = f
 toFunction p = undefined
 
-toFunction2 :: Primitive -> (Primitive -> Primitive)
+toFunction2 :: (Primitive a) -> ((Primitive a) -> (Primitive a))
 toFunction2 f = function_ (toFunction f)
 
-toInt :: Primitive -> Int
+toInt :: (Primitive a) -> Int
 toInt (Int0 i) = i
 toInt p = undefined
 
@@ -48,7 +48,7 @@ getOrFail :: Maybe a -> a
 getOrFail (Just x) = x
 getOrFail Nothing = error "This didin't happen"
 
-createCompiler :: [String] -> Expression a -> [Primitive] -> Primitive
+createCompiler :: [String] -> Expression a -> [(Primitive a)] -> (Primitive a)
 createCompiler nameStack {-exp-} =
     rec {-exp-} where
     rec exp = case exp of
@@ -70,5 +70,5 @@ createCompiler nameStack {-exp-} =
                         \env -> Fun0 (Fun "hello" (\arg -> exp0 (arg : env)))
 
 
-eval :: Expression a -> Primitive
+eval :: Expression a -> (Primitive a)
 eval input = createCompiler [] input []
