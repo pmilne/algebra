3a6fac81481081cebf48174c6707abe2eeed6ce4
pmilne <philip@pmilne.net>
2017-06-23 19:06:51 -0700

    Tidy.

--- src/Expression.hs
+++ src/Expression.hs
@@ -179,29 +179,28 @@ derivative (Fun f)         = let var = Var "d" in var ~> (derivative_ f var)
 derivative e               = error $ "Error: dd " ++ show e
 
 inverse :: (Show a, Eq a, Field a, Exponentiative a, Applicable a) => Expression a -> Expression a
-inverse (Lambda var body) =
-          Lambda var (rec body) where
+inverse (Lambda var body) = rec body where
                     rec e =
                       let vName = varName var in case e of
                           (Const _)               -> undefined
-                          (Var x)                 -> Var x
-                          (App f a)               -> apply (var ~> rec a) (apply (inverse f) var)
+                          (Var x)                 -> var ~> Var x
+                          (App f a)               -> var ~> apply (rec a) (apply (inverse f) var)
 
-                          (Sum (Const a) b)       -> apply (var ~> rec b) (neg (Const a) + var)
-                          (Sum a (Const b))       -> apply (var ~> rec a) (var - Const b)
+                          (Sum (Const a) b)       -> var ~> apply (rec b) (neg (Const a) + var)
+                          (Sum a (Const b))       -> var ~> apply (rec a) (var - Const b)
                           (Sum _ _)               -> undefined
 
-                          (Neg a)                 -> apply (var ~> rec a) (neg var)
+                          (Neg a)                 -> var ~> apply (rec a) (neg var)
 
-                          (Prd (Const a) b)       -> apply (var ~> rec b) (inv (Const a) * var)
-                          (Prd a (Const b))       -> apply (var ~> rec a) (var / Const b)
+                          (Prd (Const a) b)       -> var ~> apply (rec b) (inv (Const a) * var)
+                          (Prd a (Const b))       -> var ~> apply (rec a) (var / Const b)
                           (Prd _ _)               -> undefined
 
-                          (Inv a)                 -> apply (var ~> rec a) (inv var)
+                          (Inv a)                 -> var ~> apply (rec a) (inv var)
 
-                          (Pow a (Const n))       -> apply (var ~> rec a) (var ^ inv (Const n))
-                          (Pow (Const a) n)       -> apply (var ~> rec n)  (log (Const a) var)
+                          (Pow a (Const n))       -> var ~> apply (rec a) (var ^ inv (Const n))
+                          (Pow (Const a) n)       -> var ~> apply (rec n)  (log (Const a) var)
                           (Pow _ _)               -> undefined
-inverse (Fun f)         = let var = Var "x" in var ~> (inverse_ f var)
+inverse (Fun f)         = let var = Var "x" in var ~> inverse_ f var
 inverse e               = error $ "Error: inverse " ++ show e
 
