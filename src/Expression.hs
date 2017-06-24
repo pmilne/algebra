12c01a7f1f18d9d4e588aca847bbca25bb86fffa
pmilne <philip@pmilne.net>
2017-06-23 20:03:36 -0700

    Tidy.

--- src/Expression.hs
+++ src/Expression.hs
@@ -180,26 +180,26 @@ derivative e               = error $ "Error: dd " ++ show e
 
 inverse :: (Show a, Eq a, Field a, Exponentiative a, Applicable a) => Expression a -> Expression a
 inverse (Lambda var body) = rec body where
-                    rec e = case e of
-                          (Const _)               -> undefined
-                          (Var x)                 -> var ~> var
-                          (App f a)               -> var ~> apply (rec a) (apply (inverse f) var)
+                            rec e = case e of
+                                        (Const _)               -> undefined
+                                        (Var x)                 -> var ~> var
+                                        (App f a)               -> var ~> apply (rec a) (apply (inverse f) var)
 
-                          (Sum (Const a) b)       -> var ~> apply (rec b) (neg (Const a) + var)
-                          (Sum a (Const b))       -> var ~> apply (rec a) (var - Const b)
-                          (Sum _ _)               -> undefined
+                                        (Sum (Const a) b)       -> var ~> apply (rec b) (neg (Const a) + var)
+                                        (Sum a (Const b))       -> var ~> apply (rec a) (var - Const b)
+                                        (Sum _ _)               -> undefined
 
-                          (Neg a)                 -> var ~> apply (rec a) (neg var)
+                                        (Neg a)                 -> var ~> apply (rec a) (neg var)
 
-                          (Prd (Const a) b)       -> var ~> apply (rec b) (inv (Const a) * var)
-                          (Prd a (Const b))       -> var ~> apply (rec a) (var / Const b)
-                          (Prd _ _)               -> undefined
+                                        (Prd (Const a) b)       -> var ~> apply (rec b) (inv (Const a) * var)
+                                        (Prd a (Const b))       -> var ~> apply (rec a) (var / Const b)
+                                        (Prd _ _)               -> undefined
 
-                          (Inv a)                 -> var ~> apply (rec a) (inv var)
+                                        (Inv a)                 -> var ~> apply (rec a) (inv var)
 
-                          (Pow a (Const n))       -> var ~> apply (rec a) (var ^ inv (Const n))
-                          (Pow (Const a) n)       -> var ~> apply (rec n)  (log (Const a) var)
-                          (Pow _ _)               -> undefined
+                                        (Pow a (Const n))       -> var ~> apply (rec a) (var ^ inv (Const n))
+                                        (Pow (Const a) n)       -> var ~> apply (rec n)  (log (Const a) var)
+                                        (Pow _ _)               -> undefined
 inverse (Fun f)         = let var = Var "x" in var ~> inverse_ f var
 inverse e               = error $ "Error: inverse " ++ show e
 
